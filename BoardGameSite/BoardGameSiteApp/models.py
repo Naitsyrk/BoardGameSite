0ac3d4d02c11528560077df3550e5c6e9cc6abd6
Naitsyrk <wierzbickikrystian@gmail.com>
2022-03-16 23:00:39 +0100

    models Game, Category, Mechanics, Publishing House created

--- BoardGameSite/BoardGameSiteApp/models.py
+++ BoardGameSite/BoardGameSiteApp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(null=True)
+
+
+class Mechanics(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(null=True)
+
+
+class PublishingHouse(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=64)
+    categories = models.ManyToManyField(Category)
+    mechanics = models.ManyToManyField(Mechanics)
+    minimum_players = models.SmallIntegerField()
+    maximum_players = models.SmallIntegerField()
+    publishing_house = models.ForeignKey(PublishingHouse, on_delete=models.CASCADE)
+    minimum_age = models.SmallIntegerField()
+
