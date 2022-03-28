194df4655fbc69dbb0dd2a55f9ccec0cb6317e13
Naitsyrk <wierzbickikrystian@gmail.com>
2022-03-28 18:01:46 +0200

    in progress2

--- BoardGameSite/BoardGameSiteApp/views.py
+++ BoardGameSite/BoardGameSiteApp/views.py
@@ -9,7 +9,7 @@ from random import choice, sample
 
 from .models import Game, PublishingHouse, Category, Mechanic
 from .form import GameAddForm
-from .filter import GameFilter
+from .filter import GameFilter, RandomGameFilter
 
 class LandingPage(View):
     def get(self, request):
@@ -129,17 +129,21 @@ class random_game(View):
 
 
 class RandomGamesListView(View):
-    def get(self, request):
-        f = GameFilter(request.GET, queryset=Game.objects.all())
-        return render(request, 'random_filter_page.html', {'filter': f})
+    filter_class = RandomGameFilter
 
-    def post(self, request):
-        filter = GameFilter(request.POST)
-        # random_games_number = filter.form['games_number']
-        random_games_number = 1
-        games_number = len(filter.qs)
-        if random_games_number > games_number:
-            games = filter.qs
+    def get(self, request):
+        game_filter = self.filter_class(request.GET, queryset=Game.objects.all())
+        if game_filter.is_valid():
+            random_games_number = game_filter.form.cleaned_data['games_number']
         else:
-            games = sample(list(filter.qs), k=random_games_number)
-        return render(request, 'random_game_list.html', {'games': games})
+            random_games_number = 1
+        try:
+            random_games_number = int(random_games_number)
+        except TypeError:
+            random_games_number = 1
+        game_list_len = len(game_filter.qs)
+        if random_games_number > game_list_len:
+            games = game_filter.qs
+        else:
+            games = game_filter.qs.order_by('?')[0:random_games_number]
+        return render(request, 'random_filter_page.html', {'filter': game_filter, 'games': games})
